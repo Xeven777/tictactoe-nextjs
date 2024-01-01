8491ffb3fbfa7c662618ffb9f520325d37434823
Anish <xevenbiswas@gmail.com>
2024-01-01 20:06:59 +0530

    Refactor Tic-Tac-Toe game logic and add draw condition

--- src/app/components/Box.tsx
+++ src/app/components/Box.tsx
@@ -6,20 +6,25 @@ const Box = () => {
   const [turn, setTurn] = useState("X");
   const [count, setCount] = useState(0);
   const [boxes, setBoxes] = useState(Array(9).fill(""));
-
+  const [end, setEnd] = useState(false);
+  let message;
   useEffect(() => {
     const winner = checkWinner();
     if (winner) {
       alert(`${winner} wins!`);
-      //   setBoxes(Array(9).fill(""));
-      //   setTurn("X");
+      setEnd(true);
     }
   }, [boxes]);
 
   function handleclick(i: number) {
-    setCount(count + 1);
+    const newCount = count + 1;
     const newBoxes = boxes.slice();
     newBoxes[i] = turn;
+    setCount(newCount);
+    if (count === 8) {
+      setEnd(true);
+      console.log(end);
+    }
     setBoxes(newBoxes);
     setTurn(turn === "X" ? "O" : "X");
   }
@@ -45,24 +50,36 @@ const Box = () => {
   }
 
   function resetGame() {
+    setEnd(false);
+    setCount(0);
     setBoxes(Array(9).fill(""));
     setTurn("X");
   }
+  if (count <= 8 && !end) {
+    message = `${turn}'s turn`;
+  } else {
+    message = "Game Over. Its a Draw";
+  }
   return (
     <div className="p-2 flex flex-col">
+      <div className="my-3 text-center text-lg font-semibold text-slate-200">
+        {message}
+      </div>
+
       <div className="grid grid-cols-3 gap-2">
         {boxes.map((box, i) => (
           <button
             key={i}
             type="button"
-            className="w-20 h-20 shadow shadow-slate-500 bg-slate-300 text-slate-900 font-bold text-4xl rounded-md active:scale-95 hover:bg-slate-400 transition-all"
+            className="w-20 h-20 shadow shadow-slate-500 bg-slate-300 text-slate-900 font-bold text-4xl rounded-md active:scale-95 hover:bg-slate-200 transition-all"
             onClick={() => handleclick(i)}
-            disabled={box !== ""}
+            disabled={box !== "" || end}
           >
             {box}
           </button>
         ))}
       </div>
+
       <button
         type="button"
         className="mt-8 mx-auto px-4 py-2 bg-slate-200 text-slate-700 font-semibold rounded-md active:scale-95 hover:bg-slate-400 transition-all"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-4 p-2">
       <h2 className="text-3xl md:text-4xl font-bold p-10 selection:text-zinc-900 selection:bg-slate-400">
-        TicTacToe
+        Tic-Tac-Toe
       </h2>
       <Box />
       <div className="fixed bottom-4 right-8 text-slate-200p">
